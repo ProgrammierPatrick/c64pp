a2f8d93ad677954e4bcc8d5293dbb6617eae7779
MRiedel <mriedelx15@gmail.com>
2022-01-18 19:35:26 +0100

    added other graphic modes

--- src/emu/vic/background_graphics.cpp
+++ src/emu/vic/background_graphics.cpp
@@ -33,3 +33,107 @@ std::array<uint8_t, 8> BackgroundGraphics::standardTextModeGAccess() {
     }
     return pixels;
 }
+
+std::array<uint8_t, 8> BackgroundGraphics::multicolorTextModeGAccess() {
+    auto c = vic->videoMatrixLine[vic->VMLI];
+    auto g = vic->accessMem(((vic->charGenMemoryPosition & 0x7) << 11) | 0x1000 | (c.val << 3) | vic->RC).val;
+
+    std::array<uint8_t, 8> pixels = { 0 };
+    if ((c.col & 0x8) == 0) {
+        for (int i = 0; i < 8; i++) {
+            pixels[i] = g & (1 << (7 - i)) ? (c.col & 0x7) : vic->backgroundColors[0];
+        }
+    }
+    else {
+        for (int i = 0; i < 4; i++) {
+            if (((g >> (6 - 2 * i)) & 0x3) == 0x0) pixels[2*i] = pixels[2*i+1] = vic->backgroundColors[0];
+            if (((g >> (6 - 2 * i)) & 0x3) == 0x1) pixels[2*i] = pixels[2*i+1] = vic->backgroundColors[1];
+            if (((g >> (6 - 2 * i)) & 0x3) == 0x2) pixels[2*i] = pixels[2*i+1] = vic->backgroundColors[2];
+            if (((g >> (6 - 2 * i)) & 0x3) == 0x3) pixels[2*i] = pixels[2*i+1] = c.col & 0x7;
+        }
+    }
+    return pixels;
+}
+
+std::array<uint8_t, 8> BackgroundGraphics::standardBitMapModeGAccess() {
+    auto c = vic->videoMatrixLine[vic->VMLI];
+    auto g = vic->accessMem(((vic->charGenMemoryPosition & 0x4) << 11) | 0x1000 | ((vic->VC & 0x3FF) << 3) | vic->RC).val;
+
+    std::array<uint8_t, 8> pixels = { 0 };
+    for (int i = 0; i < 8; i++) {
+        pixels[i] = g & (1 << (7 - i)) ? (c.val >> 4) : (c.val & 0x0F);
+    }
+    return pixels;
+}
+
+std::array<uint8_t, 8> BackgroundGraphics::multicolorBitMapModeGAccess() {
+    auto c = vic->videoMatrixLine[vic->VMLI];
+    auto g = vic->accessMem(((vic->charGenMemoryPosition & 0x4) << 11) | 0x1000 | ((vic->VC & 0x3FF) << 3) | vic->RC).val;
+
+    std::array<uint8_t, 8> pixels = { 0 };
+    for (int i = 0; i < 4; i++) {
+        if (((g >> (6 - 2 * i)) & 0x3) == 0x0) pixels[2*i] = pixels[2*i+1] = vic->backgroundColors[0];
+        if (((g >> (6 - 2 * i)) & 0x3) == 0x1) pixels[2*i] = pixels[2*i+1] = (c.val >> 4);
+        if (((g >> (6 - 2 * i)) & 0x3) == 0x2) pixels[2*i] = pixels[2*i+1] = (c.val & 0x0F);
+        if (((g >> (6 - 2 * i)) & 0x3) == 0x3) pixels[2*i] = pixels[2*i+1] = c.col;
+    }
+    return pixels;
+}
+
+std::array<uint8_t, 8> BackgroundGraphics::ECMTextModeGAccess() {
+    auto c = vic->videoMatrixLine[vic->VMLI];
+    auto g = vic->accessMem(((vic->charGenMemoryPosition & 0x7) << 11) | 0x1000 | (c.val & 0x3F) << 3 | vic->RC).val;
+
+    std::array<uint8_t, 8> pixels = { 0 };
+    for (int i = 0; i < 8; i++) {
+        if ((g & (1 << (7 - i))) == 0) {
+            pixels[i] = vic->backgroundColors[(c.val >> 6) & 0x3];
+        }
+        else {
+            pixels[i] = c.col;
+        }
+    }
+    return pixels;
+}
+
+std::array<uint8_t, 8> BackgroundGraphics::invalidTextModeGAccess() {
+    auto c = vic->videoMatrixLine[vic->VMLI];
+    auto g = vic->accessMem(((vic->charGenMemoryPosition & 0x7) << 11) | 0x1000 | (c.val & 0x3F) << 3 | vic->RC).val;
+
+    std::array<uint8_t, 8> pixels = { 0 };
+    if ((c.col & 0x8) == 0) {
+        for (int i = 0; i < 8; i++) {
+            pixels[i] = 0; // todo: differentiate between fore- and background
+        }
+    }
+    else {
+        for (int i = 0; i < 4; i++) {
+            if ((g >> (6 - 2 * i)) == 0x0 || (g >> (6 - 2 * i)) == 0x1) pixels[2*i] = pixels[2*i+1] = 0; // todo: background
+            if ((g >> (6 - 2 * i)) == 0x2 || (g >> (6 - 2 * i)) == 0x3) pixels[2*i] = pixels[2*i+1] = 0; // todo: foreground
+        }
+    }
+    return pixels;
+}
+
+std::array<uint8_t, 8> BackgroundGraphics::invalidBitMapMode1GAccess() {
+    auto c = vic->videoMatrixLine[vic->VMLI];
+    auto g = vic->accessMem(((vic->charGenMemoryPosition & 0x4) << 11) | 0x1000 | (vic->VC & 0x33F) << 3 | vic->RC).val;
+
+    std::array<uint8_t, 8> pixels = { 0 };
+    for (int i = 0; i < 8; i++) {
+        pixels[i] = 0; // todo: differentiate between fore- and background
+    }
+    return pixels;
+}
+
+std::array<uint8_t, 8> BackgroundGraphics::invalidBitMapMode2GAccess() {
+    auto c = vic->videoMatrixLine[vic->VMLI];
+    auto g = vic->accessMem(((vic->charGenMemoryPosition & 0x4) << 11) | 0x1000 | (vic->VC & 0x33F) << 3 | vic->RC).val;
+
+    std::array<uint8_t, 8> pixels = { 0 };
+    for (int i = 0; i < 4; i++) {
+        if ((g >> (6 - 2 * i)) == 0x0 || (g >> (6 - 2 * i)) == 0x1) pixels[2*i] = pixels[2*i+1] = 0; // todo: background
+        if ((g >> (6 - 2 * i)) == 0x2 || (g >> (6 - 2 * i)) == 0x3) pixels[2*i] = pixels[2*i+1] = 0; // todo: foreground
+    }
+    return pixels;
+}
--- src/emu/vic/background_graphics.h
+++ src/emu/vic/background_graphics.h
@@ -13,6 +13,13 @@ public:
     std::array<uint8_t, 8> gAccess();
 
     std::array<uint8_t, 8> standardTextModeGAccess();
+    std::array<uint8_t, 8> multicolorTextModeGAccess();
+    std::array<uint8_t, 8> standardBitMapModeGAccess();
+    std::array<uint8_t, 8> multicolorBitMapModeGAccess();
+    std::array<uint8_t, 8> ECMTextModeGAccess();
+    std::array<uint8_t, 8> invalidTextModeGAccess();
+    std::array<uint8_t, 8> invalidBitMapMode1GAccess();
+    std::array<uint8_t, 8> invalidBitMapMode2GAccess();
 
 public:
     VIC *vic;
